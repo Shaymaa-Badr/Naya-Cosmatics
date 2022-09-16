5554dadcab5238911e4bdc3994cb2853d5fd629e
unknown <shaymaa.badr.g@gmail.com>
2022-09-16 22:09:47 +0400

    CSS styling

--- js/script.js
+++ js/script.js
@@ -93,16 +93,14 @@ const hairprouductsEn = './assets/img/Naya_hair_products_img_en.png'
 const loader = document.querySelector('.loader-overlay');
 const main = document.querySelector('body');
 
-function init() {
+//Set loader overlay
+function loading() {
   setTimeout(() => {
     loader.style.opacity = 0;
     loader.style.display = 'none';
-
-    main.style.display = 'block';
-    setTimeout(() => (main.style.opacity = 1), 50);
-  }, 4000);
+  }, 10000);
 }
-init()
+loading()
 
 // Collapse navbar
 const navBtnToggle = () => {
